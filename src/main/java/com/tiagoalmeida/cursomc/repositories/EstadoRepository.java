ad0566a4ef20dbd844ada344b9fa2d3aa627707e
@TiagoAlmeidaPoa <tiago.pipinha@gmail.com>
2021-01-13 14:53:06 -0300

    feat(Estado e Cidade) cria Estado e Cidade e suas associacoes

---
+++ src/main/java/com/tiagoalmeida/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.tiagoalmeida.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tiagoalmeida.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
